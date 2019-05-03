e988a55e143820f6f2edb19b9902647da70884e8
Scott Tadman <tadman@postageapp.com>
2019-05-03 12:34:33 -0400

    Basic implementation of prototype

--- lib/configature.rb
+++ lib/configature.rb
@@ -4,7 +4,43 @@ module Configature
   class Error < StandardError; end
   # Your code goes here...
 
-  def version
+  def self.version
     VERSION
+  end
+
+  def self.dir_plus_parents(dir)
+    Enumerator.new do |y|
+      y << dir
+
+      loop do
+        last, dir = dir, File.expand_path('../', dir)
+
+        break if (last == dir)
+
+        y << dir
+      end
+    end
+  end
+
+  def self.config_dir
+    dir_plus_parents(Dir.pwd).lazy.map do |dir|
+      File.expand_path('config', dir)
+    end.find do |dir|
+      File.directory?(dir)
+    end
+  end
+
+  def self.configable_examples(dir)
+    map = Dir.glob(File.expand_path('*.example', dir)).map do |source|
+      [ source, source.delete_suffix('.example') ]
+    end.to_h
+
+    if (block_given?)
+      map.each do |k,v|
+        yield(k, v)
+      end
+    end
+
+    map
   end
 end
--- spec/configature_spec.rb
+++ spec/configature_spec.rb
@@ -3,7 +3,21 @@ RSpec.describe Configature do
     expect(Configature::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context 'can properly enumerate parent directories' do
+    it 'on a POSIX system' do
+      path = '/a/very/long/path/to/something'
+
+      dirs = Configature.dir_plus_parents(path).to_a
+
+      expect(dirs).to eq(%w[
+        /a/very/long/path/to/something
+        /a/very/long/path/to
+        /a/very/long/path
+        /a/very/long
+        /a/very
+        /a
+        /
+      ])
+    end
   end
 end
